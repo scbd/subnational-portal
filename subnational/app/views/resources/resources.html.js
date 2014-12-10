7d46b9b7cab833c350ae66431b6eb2c93eb4117a
Nancy Liang <nancy.liang@cbd.int>
2014-12-10 11:57:54 -0500

    modifications on pages

--- subnational/app/views/resources/resources.html.js
+++ subnational/app/views/resources/resources.html.js
@@ -4,11 +4,11 @@ require('app').controller('ResourcesPageController', ['$scope', '$rootScope', fu
 	$rootScope.homePage = false;
 	$rootScope.portal = 'resources';
 	$rootScope.navigation = [
-			{ url: '/subnational/resources/decisions'        			, title: 'Decisions' },
-			{ url: '/subnational/resources/strategies-and-action-plans'	, title: 'Strategies and Action Plans' },
-			{ url: '/subnational/resources/documents'        			, title: 'Documents' },
-			{ url: '/subnational/resources/case-studies'        			, title: 'Case Studies' },
-			{ url: '/subnational/partners-and-initiatives/cbo'        		, title: 'CBO' },
-			{ url: '/subnational/resources/additional-information'       , title: 'Additional Information'}
+	//		{ url: '/subnational/resources/decisions'        			, title: 'Decisions' },
+	//		{ url: '/subnational/resources/strategies-and-action-plans'	, title: 'Strategies and Action Plans' },
+	//		{ url: '/subnational/resources/documents'        			, title: 'Documents' },
+	//		{ url: '/subnational/resources/case-studies'        			, title: 'Case Studies' },
+	//		{ url: '/subnational/partners-and-initiatives/cbo'        		, title: 'CBO' },
+	//		{ url: '/subnational/resources/additional-information'       , title: 'Additional Information'}
 		];
 }]);
