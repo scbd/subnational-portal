1e251833741f3c86d7e0878789c3dcf2c3734c04
Philippe Leblond <leblond.philippe@gmail.com>
2015-05-21 11:45:16 -0400

    Bugfix to deployment procedure

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-export DOCKER_HOST=tcp://localhost:2375
-rm -rf node_modules
-docker build -t registry.infra.cbd.int:5000/subnational-portal .
+docker build -t registry.infra.cbd.int:5000/subnational-portal git@github.com:scbd/subnational.cbd.int.git
 docker push registry.infra.cbd.int:5000/subnational-portal
