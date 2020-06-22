71b34bb25762e64ed87cce5655ea6d96887029f2
Vince Gopez <vince.gopez@cbd.int>
2020-06-22 12:36:24 -0400

    updated circleci config and carousel.

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
+'use strict';
+/* jshint node:true */
 var fs = require('fs');
 var http = require('http');
 var express = require('express');
 var httpProxy = require('http-proxy');
 
-// CREATE HTTP SERVER AND PROXY
-        
+// Create server
+
 var app = express();
 var server = http.createServer(app);
-var proxy = new httpProxy.RoutingProxy();
+var oneDay = 24*60*60*1000;
 
-// LOAD CONFIGURATION
+app.use('/subnational/app', express.static(__dirname + '/subnational/app'));
 
-var oneDay = 86400000;
+// Configure routes
 
-app.configure(function() {
-    app.set('port', process.env.PORT || 8000, '127.0.0.1');
-    app.use('/subnational/app/libs',         express.static(__dirname + '/subnational/app/libs', { maxAge: 28*oneDay }));
-    app.use('/subnational/app',              express.static(__dirname + '/subnational/app'));
-});
-
-// SET ROUTES
+var proxy = httpProxy.createProxyServer({});
 
 app.get   ('/subnational/app/*', function(req, res) { res.send('404', 404); } );
+app.get   ('/public/*',   function(req, res) { res.send('404', 404); } );
+app.all   ('/api/*',      function(req, res) { proxy.web(req, res, { target: 'https://api.cbd.int', secure: false } ); } );
 
-app.get   ('/api/*', function(req, res) { proxy.web(req, res, { target: 'https://api.cbd.int', secure: false } ); } );
-app.put   ('/api/*', function(req, res) { proxy.web(req, res, { target: 'https://api.cbd.int', secure: false } ); } );
-app.post  ('/api/*', function(req, res) { proxy.web(req, res, { target: 'https://api.cbd.int', secure: false } ); } );
-app.delete('/api/*', function(req, res) { proxy.web(req, res, { target: 'https://api.cbd.int', secure: false } ); } );
-
-
-// SET TEMPLATE
+// Configure index.html
 
 app.get('/subnational*', function(req, res) {
-	fs.readFile(__dirname + '/subnational/app/template.html', 'utf8', function (error, text) { 
-		res.send(text); 
+	fs.readFile(__dirname + '/subnational/app/template.html', 'utf8', function (error, text) {
+		res.send(text);
 	});
 });
 
 // START HTTP SERVER
 
-console.log('Server listening on port ' + app.get('port') + '.');
-server.listen(app.get('port'));
+app.listen(process.env.PORT || 8000, '0.0.0.0', function () {
+	console.log('Server listening on %j', this.address());
+});
+
+// Handle proxy errors ignore
+
+proxy.on('error', function (e,req, res) {
+    console.error('proxy error:', e);
+    res.status(502).send();
+});
